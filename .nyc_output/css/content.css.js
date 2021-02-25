4e27ee886cf2cce0590527c266b4abe6dc747e23
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-02-25 09:52:08 +0100

    .

---
+++ .nyc_output/css/content.css.js
@@ -0,0 +1,14 @@
+form {
+  width: 90vw;
+  border: 0.1rem solid #ccc;
+  padding: 2%;
+  margin: 5% auto;
+  box-sizing: border-box;
+}
+
+
+/* Should be found by coverage */
+.uncovered {
+  background: red;
+}
+
--- tests/puppeteer.js
+++ tests/puppeteer.js
@@ -4,7 +4,7 @@ const pti = require('puppeteer-to-istanbul');
 
 (async () => {
 
-  const url    = 'http://127.0.0.1/tests/e2e-testing/src';
+  const url    = 'http://localhost:8080';
   const config = {
     headless : false, // Default: true
     slowMo   : 250,
